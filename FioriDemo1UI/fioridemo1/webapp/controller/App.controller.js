a87d0fbff98188c1e49c5acd63838f247d9676f1
sapui5 training99 <sapui5training99@gmail.com>
2025-01-16 06:01:25 +0000

    First commit

---
+++ FioriDemo1UI/fioridemo1/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("myfiroidemo.fioridemo1.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ FioriDemo1UI/fioridemo1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"myfiroidemo/fioridemo1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
